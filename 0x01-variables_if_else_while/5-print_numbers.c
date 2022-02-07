a7beeb4ba4a0ebe333c2debec60715d8adcddcae
Icarus399 <3530@holbertonschool.com>
2022-02-07 14:19:16 +0000

    adding file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints all single digit numbers if base 10
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i > 10; i++)
+	{
+		putchar('\n');
+	}
+	return (0);
+}
