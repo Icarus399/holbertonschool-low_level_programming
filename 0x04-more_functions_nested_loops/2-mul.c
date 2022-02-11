dabecfa6cf265e925c0644531cc2430032f57f6b
Icarus399 <3530@holbertonschool.com>
2022-02-11 02:43:04 +0000

    adding file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplication function
+ * Return: 0
+ * @a: interger
+ * @b: int
+ */
+
+int mul(int a, int b)
+{
+	int math;
+
+	math = a * b;
+
+	return (math);
+}
