14343d3c335bec28b2c8de95cce25d5ea6681242
Icarus399 <3530@holbertonschool.com>
2022-02-11 01:50:22 +0000

    fix

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - this program will assing a random number to the variable.
+ * Return: 0
+ */
+
+void positive_or_negative(int n)
+{
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+
+	}
+
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+
+}
+
