4fccec864981cb84051fc0d427e520c77a9db095
Icarus399 <3530@holbertonschool.com>
2022-02-11 02:29:24 +0000

    adding file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - function that checks for 1 number digits
+ * @c: interger
+ * Return: 0
+ *
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
+
