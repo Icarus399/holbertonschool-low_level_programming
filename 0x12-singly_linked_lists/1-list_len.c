07569d1007120232450b831b96392e303119c751
Icarus399 <3530@holbertonschool.com>
2022-03-19 02:53:49 +0000

    add file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - function that returns the lenght 
+ * @h: the head
+ * Return: len
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
