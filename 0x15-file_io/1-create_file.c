19e2f0f097c94bed42e00881cce2f1678635a4e4
Icarus399 <3530@holbertonschool.com>
2022-03-29 02:43:38 +0000

    adding files for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @text_content: NULL terminated string to write to file
+ * Return: 1 on succerss, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, _write;
+
+	len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (!text_content)
+	{
+		text_content = "";
+	}
+	while (text_content[len])
+	{
+		len++;
+	}
+	_write = write(fd, text_content, len);
+	if (_write == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
